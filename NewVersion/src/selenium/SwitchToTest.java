b41a0ec3529d4eb82e5c5cc2c2f89124dab86d6e
NaveenDillikumar <151106161+NaveenDillikumar@users.noreply.github.com>
2024-01-20 15:15:55 +0530

    Testing Program

---
+++ NewVersion/src/selenium/SwitchToTest.java
@@ -0,0 +1,28 @@
+package selenium;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SwitchToTest {
+	
+	public static void main(String[] args)throws Exception {
+	
+		//Launch the Browser
+		WebDriver driver=new ChromeDriver();
+		
+		//Maximize the browser
+		driver.manage().window().maximize();
+		
+		//Navigate to Webpage
+		driver.get("https://Google.com");
+		Thread.sleep(3000);
+		
+		driver.switchTo().activeElement().sendKeys("Lord Muruga",Keys.ENTER);
+		Thread.sleep(5000);
+		
+		//Close the Browser
+		driver.quit();
+	}
+
+}
